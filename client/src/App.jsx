5e8903fe1848b540b84d3ffbb1fc2e3fd19794e2
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2024-09-28 22:09:35 -0300

    Cositas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,58 @@
 import './App.css'
+import Login from './components/Login/Login'
+import Dashboard from './components/Dashboard/Dashboard'
+import {
+  createBrowserRouter,
+  RouterProvider
+}from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <div></div>
+  },
+  {
+    path:'/sistemasgpi',
+    element: <div><Dashboard></Dashboard></div>
+  },
+  {
+    path:'/iniciarsesion',
+    element: <div><Login></Login></div>
+  }
+])
 function App() {
 
   return (
-    <>
-      <div>
-        App
+      <div> 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+              <img src="./assets/logo.png" width='30' alt='logoUCP'></img>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+        </nav>
+        <RouterProvider router={router}>
+          <Dashboard></Dashboard>
+          <Login></Login>
+        </RouterProvider>
       </div>
-    </>
   )
 }
 
---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Dashboard.css'
+
+function Dashboard() {
+  return (
+    <div>
+        This is a Dashboard Page
+        <a href='/login'> Login </a> 
+        <a href='/'>LogOut </a>
+    </div>
+  )
+}
+
+export default Dashboard
