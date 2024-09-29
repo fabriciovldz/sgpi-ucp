dea4173976576088ff183687a39cb19512323779
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2024-09-28 22:31:50 -0300

    mas cambios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import logoUCP from './assets/iconUCP.png';
 import Login from './components/Login/Login'
 import Dashboard from './components/Dashboard/Dashboard'
 import {
@@ -26,7 +27,7 @@ function App() {
       <div> 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">
-              <img src="./assets/logo.png" width='30' alt='logoUCP'></img>
+              <img src={logoUCP} width="40" alt='logoUCP' style={{marginLeft:'20px'}}></img>
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
@@ -34,16 +35,16 @@ function App() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <a className="nav-link" href="/formularios"> Formularios <span className="sr-only">(Actualmente)</span></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Features</a>
+                  <a className="nav-link" href="/reportes"> Reportes </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Pricing</a>
+                  <a className="nav-link" href="/ayuda"> Ayuda </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link disabled" href="#">Disabled</a>
+                  <a className="nav-link disabled" href="/admin"> Administrador </a>
                 </li>
               </ul>
             </div>
